2667a4d6edfe94c1a4ff7535886d81086011d161
Akhil Kancherla <akhilkancherla@Akhils-MacBook-Pro.local>
2021-10-20 18:26:44 -0400

    adding an old lab

---
+++ Calculator.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // Receive first number
+        System.out.print("Enter first operand: ");
+        double first = scan.nextDouble();
+
+        //Receive second number
+        System.out.print("Enter second operand: ");
+        double second = scan.nextDouble();
+
+        //Print calculator menu
+        System.out.println("");
+        System.out.println("Calculator Menu");
+        System.out.println("---------------");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+        System.out.println("");
+
+        //Ask user which operation to perform on the 2 numbers
+        System.out.print("Which operation do you want to perform? ");
+        int operation = scan.nextInt();
+
+        //if statement to see which operation needs to be executed
+        if(operation == 1){
+            System.out.println("The result of the operation is " + (first + second) + ". Goodbye!");
+        } else if(operation == 2){
+            System.out.println("The result of the operation is " + (first - second) + ". Goodbye!");
+        } else if(operation == 3){
+            System.out.println("The result of the operation is " + (first * second) + ". Goodbye!");
+        } else if(operation == 4){
+            System.out.println("The result of the operation is " + (first / second) + ". Goodbye!");
+        } else{
+            System.out.println("Error: Invalid selection! Terminating program.");
+        }
+
+    }
+}
